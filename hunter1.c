90000ecdbfdd9ff0a39e2423d5a2ac85527a33b6
Sathish Gnanaprakasam <hsithas@users.noreply.github.com>
2017-02-25 03:23:54 +0530

    Update hunter1.c

--- hunter1.c
+++ hunter1.c
@@ -2,5 +2,20 @@
 #include <stdio.h>
 int main()
 {
+  int n;
+  int *a;
+  int hash[100000] = {0};
+  scanf("%d",&n);
+  a = (int*)malloc(sizeof(int)*n);
+  for(int i=0;i<n;i++)
+    scanf("%d",(a+i)),hash[a[i]]++;
+  for(int i=0;i<n;i++)
+  {
+    if(hash[a[i]]>1)
+    {
+      printf("%d ",a[i]);
+      hash[a[i]] = 0;
+    }
+  }
   return 0;
 }
