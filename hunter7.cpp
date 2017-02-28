3824b6fec4b2697e9939186871f36515f2de5253
Sathish Gnanaprakasam <hsithas@users.noreply.github.com>
2017-02-28 17:42:07 +0530

    Create hunter7.cpp

---
+++ hunter7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int x;
+	int result = 0;
+	for(int i=0;i<n;i++)
+	{
+		cin>>x;result^=x;
+	}
+	cout<<result<<endl;
+	return 0;
+}
